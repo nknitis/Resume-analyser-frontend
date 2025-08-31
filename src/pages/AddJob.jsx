4247789f0551896b6a82a99d7818297b47b9a728
Nitish <nkcode.789@gmail.com>
2025-08-31 23:55:44 +0530

    Update AddJob.jsx

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://resume-analyser-backend-2gcm.onrender.com/api";
 
 export default function AddJob() {
   const [jobId, setJobId] = useState("");
